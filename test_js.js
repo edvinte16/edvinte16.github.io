018ce90e404e961a94b7a3234a0fde78e6117b63
edvin johansson <redeye0700@hotmail.se>
2018-05-02 13:47:02 +0200

    javascript
    @ntitobias

--- test_js.js
+++ test_js.js
@@ -1,10 +1,16 @@
 function myFunction() {
     document.getElementById("demo").innerHTML = "Paragraph changed.";
+
+    var x = 5;
+var y = 6;
+var z = x + y;
+document.getElementById("demo").innerHTML = "the total is: " + z;
+
+
+
+var pi = 3.14;
+var person = "John Doe";
+var answer = 'Yes I am!';
 }
-console.log( 5 + 6);
 
 alert("dö");
-var x,z,y;
-x = 5;
-y = 6;
-z = x + y;
