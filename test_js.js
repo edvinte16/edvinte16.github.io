d55454471e1b4b429c5b4d548a139438bc785b17
edvin johansson <redeye0700@hotmail.se>
2018-05-04 11:23:37 +0200

    javascript ändringar

--- test_js.js
+++ test_js.js
@@ -1,7 +1,14 @@
 function myFunction() {
     document.getElementById("demo").innerHTML = "Ello javascript.";
 
+    var x = 5;
+var y = 6;
+var z = x + y;
+document.getElementById("demo").innerHTML = "the total is: " + z;
+
 var pi = 3.14;
 var person = "John Doe";
 var answer = 'Yes I am!';
 }
+
+
